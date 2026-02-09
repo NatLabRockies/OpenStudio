e616e4cee85127f476dfb04439283b5f497b2a1a
Julien Marrec <julien.marrec@gmail.com>
2026-02-09 17:42:28 +0100

    RT test for FanVariableVolume

--- src/energyplus/Test/FanVariableVolume_GTest.cpp
+++ src/energyplus/Test/FanVariableVolume_GTest.cpp
@@ -7,15 +7,27 @@
 #include "EnergyPlusFixture.hpp"
 
 #include "../ForwardTranslator.hpp"
+#include "../ReverseTranslator.hpp"
 
 #include "../../model/FanVariableVolume.hpp"
+#include "../../model/FanVariableVolume_Impl.hpp"
 
 #include "../../model/Model.hpp"
-#include "../../model/Node.hpp"
-#include "../../model/AirLoopHVAC.hpp"
 
-#include <utilities/idd/Fan_VariableVolume_FieldEnums.hxx>
+#include "../../model/AirLoopHVAC.hpp"
+#include "../../model/Node.hpp"
+#include "../../model/Schedule.hpp"
+#include "../../model/ScheduleConstant.hpp"
+#include "../../model/ScheduleConstant_Impl.hpp"
+
+#include "../../utilities/idf/Workspace.hpp"
+#include "../../utilities/idf/IdfObject.hpp"
+#include "../../utilities/idf/WorkspaceObject.hpp"
+
+// E+ FieldEnums
 #include <utilities/idd/IddEnums.hxx>
+#include <utilities/idd/IddFactory.hxx>
+#include <utilities/idd/Fan_VariableVolume_FieldEnums.hxx>
 
 using namespace openstudio::energyplus;
 using namespace openstudio::model;
@@ -62,4 +74,66 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_FanVariableVolume_NotUsed) {
 
   WorkspaceObjectVector idfObjs(w.getObjectsByType(IddObjectType::Fan_VariableVolume));
   EXPECT_EQ(0u, idfObjs.size());
+}
+
+TEST_F(EnergyPlusFixture, ReverseTranslator_FanVariableVolume) {
+
+  ReverseTranslator rt;
+
+  Workspace w(StrictnessLevel::Minimal, IddFileType::EnergyPlus);
+
+  auto woFVV = w.addObject(IdfObject(IddObjectType::Fan_VariableVolume)).get();
+
+  woFVV.setName("My FanVariableVolume");
+  auto woavailabilitySchedule = w.addObject(IdfObject(IddObjectType::Schedule_Constant)).get();
+  woavailabilitySchedule.setName("My availabilitySchedule");
+  EXPECT_TRUE(woFVV.setPointer(Fan_VariableVolumeFields::AvailabilityScheduleName, woavailabilitySchedule.handle()));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanTotalEfficiency, 0.75));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::PressureRise, 0.4));
+  // Autosize
+  // EXPECT_TRUE(woFVV.setString(Fan_VariableVolumeFields::MaximumFlowRate, "Autosized"));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::MaximumFlowRate, 0.5));
+  EXPECT_TRUE(woFVV.setString(Fan_VariableVolumeFields::FanPowerMinimumFlowRateInputMethod, "Fraction"));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerMinimumFlowFraction, 0.875));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerMinimumAirFlowRate, 0.8));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::MotorEfficiency, 0.9));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::MotorInAirstreamFraction, 0.909));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerCoefficient1, 1.1));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerCoefficient2, 1.2));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerCoefficient3, 1.3));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerCoefficient4, 1.4));
+  EXPECT_TRUE(woFVV.setDouble(Fan_VariableVolumeFields::FanPowerCoefficient5, 1.5));
+  EXPECT_TRUE(woFVV.setString(Fan_VariableVolumeFields::AirInletNodeName, "FanVV Inlet Node Name"));
+  EXPECT_TRUE(woFVV.setString(Fan_VariableVolumeFields::AirInletNodeName, "Fan VV Outlet Node Name"));
+  EXPECT_TRUE(woFVV.setString(Fan_VariableVolumeFields::EndUseSubcategory, "Fan End Use Subcategory"));
+
+
+  const Model m = rt.translateWorkspace(w);
+  const auto modelObjects = m.getConcreteModelObjects<FanVariableVolume>();
+  ASSERT_EQ(1u, modelObjects.size());
+
+  const auto& modelObject = modelObjects.front();
+  ASSERT_TRUE(modelObject.availabilitySchedule().optionalCast<ScheduleConstant>());
+  EXPECT_EQ(woavailabilitySchedule.nameString(), modelObject.availabilitySchedule().nameString());
+  EXPECT_EQ(0.75, modelObject.fanTotalEfficiency());
+  EXPECT_EQ(0.4, modelObject.pressureRise());
+
+  EXPECT_FALSE(modelObject.isMaximumFlowRateAutosized());
+  ASSERT_TRUE(modelObject.maximumFlowRate());
+  EXPECT_EQ(0.5, modelObject.maximumFlowRate().get());
+
+  EXPECT_EQ("Fraction", modelObject.fanPowerMinimumFlowRateInputMethod());
+  EXPECT_EQ(0.875, modelObject.fanPowerMinimumFlowFraction());
+
+  ASSERT_TRUE(modelObject.fanPowerMinimumAirFlowRate());
+  EXPECT_EQ(0.8, modelObject.fanPowerMinimumAirFlowRate().get());
+
+  EXPECT_EQ(0.9, modelObject.motorEfficiency());
+  EXPECT_EQ(0.909, modelObject.motorInAirstreamFraction());
+  EXPECT_EQ(1.1, modelObject.fanPowerCoefficient1().get());
+  EXPECT_EQ(1.2, modelObject.fanPowerCoefficient2().get());
+  EXPECT_EQ(1.3, modelObject.fanPowerCoefficient3().get());
+  EXPECT_EQ(1.4, modelObject.fanPowerCoefficient4().get());
+  EXPECT_EQ(1.5, modelObject.fanPowerCoefficient5().get());
+  EXPECT_EQ("Fan End Use Subcategory", modelObject.endUseSubcategory());
 }
