534afff6fe3acd515a4eca0ad19b113f6b6ac884
Joe Robertson <joseph.robertson@nrel.gov>
2026-05-11 09:46:57 -0700

    Add test for alt ctor.

--- src/epmodel/test/ScheduleFile_GTest.cpp
+++ src/epmodel/test/ScheduleFile_GTest.cpp
@@ -8,11 +8,9 @@
 #include "EPModelFixture.hpp"
 #include "../ScheduleInterval/ScheduleFile.hpp"
 
-#include "../../utilities/time/Date.hpp"
-#include "../../utilities/time/TIme.hpp"
-#include "../../utilities/time/DateTime.hpp"
-#include <utilities/time/Date.hpp>
+#include "../../utilities/data/TimeSeries.hpp"
 
+using namespace openstudio;
 using namespace openstudio::epmodel;
 
 TEST_F(EPModelFixture, ScheduleFile_DefaultConstructor) {
@@ -23,6 +21,65 @@ TEST_F(EPModelFixture, ScheduleFile_DefaultConstructor) {
   EXPECT_EQ(1, schedule.columnNumber());
   EXPECT_EQ(0, schedule.rowstoSkipatTop());
 }
+
+/* TEST_F(EPModelFixture, ScheduleFileAltCtor) {
+  Model model;
+  // EXPECT_EQ(0u, model.getConcreteModelObjects<ExternalFile>().size());
+  EXPECT_EQ(0u, model.getConcreteModelObjects<ScheduleFile>().size());
+
+  path p = resourcesPath() / toPath("model/schedulefile.csv");
+  EXPECT_TRUE(exists(p));
+
+  path expectedDestDir;
+  std::vector<path> absoluteFilePaths = model.workflowJSON().absoluteFilePaths();
+  if (absoluteFilePaths.empty()) {
+    expectedDestDir = model.workflowJSON().absoluteRootDir();
+  } else {
+    expectedDestDir = absoluteFilePaths[0];
+  }
+
+  if (exists(expectedDestDir)) {
+    removeDirectory(expectedDestDir);
+  }
+  ASSERT_FALSE(exists(expectedDestDir));
+
+  ScheduleFile schedule(model, openstudio::toString(p));
+  EXPECT_EQ(1u, model.getConcreteModelObjects<ScheduleFile>().size());
+  // EXPECT_EQ(1u, model.getConcreteModelObjects<ExternalFile>().size());
+  ExternalFile externalfile = schedule.externalFile();
+  EXPECT_EQ(1u, externalfile.scheduleFiles().size());
+  EXPECT_EQ(openstudio::toString(p), externalfile.fileName());
+  //EXPECT_TRUE(externalfile.isColumnSeparatorDefaulted());
+  EXPECT_FALSE(equivalent(expectedDestDir / externalfile.fileName(), externalfile.filePath()));
+  EXPECT_TRUE(exists(externalfile.filePath()));
+  EXPECT_EQ(p, externalfile.filePath());
+  EXPECT_TRUE(schedule.isNumberofHoursofDataDefaulted());
+  EXPECT_EQ(1, schedule.columnNumber());
+  schedule.setColumnNumber(1);
+  EXPECT_EQ(1, schedule.columnNumber());
+  EXPECT_EQ(0, schedule.rowstoSkipatTop());
+  schedule.setRowstoSkipatTop(1);
+  EXPECT_EQ(1, schedule.rowstoSkipatTop());
+
+  EXPECT_EQ("Comma", schedule.columnSeparator());
+  EXPECT_TRUE(schedule.isColumnSeparatorDefaulted());
+  EXPECT_TRUE(schedule.setColumnSeparator("Tab"));
+  EXPECT_EQ("Tab", schedule.columnSeparator());
+
+  ScheduleFile schedule2(model, openstudio::toString(p));
+  EXPECT_EQ(2u, model.getConcreteModelObjects<ScheduleFile>().size());
+  EXPECT_EQ(1u, model.getConcreteModelObjects<ExternalFile>().size());
+  ExternalFile externalfile2 = schedule2.externalFile();
+  EXPECT_EQ(2u, externalfile.scheduleFiles().size());
+  EXPECT_EQ(externalfile.handle(), externalfile2.handle());
+  EXPECT_TRUE(schedule2.isNumberofHoursofDataDefaulted());
+  EXPECT_EQ(1, schedule2.columnNumber());
+  schedule2.setColumnNumber(2);
+  EXPECT_EQ(2, schedule2.columnNumber());
+  EXPECT_EQ(0, schedule2.rowstoSkipatTop());
+  schedule2.setRowstoSkipatTop(1);
+  EXPECT_EQ(1, schedule2.rowstoSkipatTop());
+} */
 
 TEST_F(EPModelFixture, ScheduleFile_ScalarAccessors_RoundTrip) {
   Model model;
@@ -87,14 +144,15 @@ TEST_F(EPModelFixture, ScheduleFile_fromTimeSeries_intervalLengthYes) {
 
   boost::optional<ScheduleFile> schedule = ScheduleFile::fromTimeSeries(timeSeries, model);
   ASSERT_TRUE(schedule);
-  EXPECT_EQ("", schedule->fileName());
+  //EXPECT_EQ("", schedule->fileName());
   EXPECT_EQ(1, schedule->columnNumber());
   EXPECT_EQ(0, schedule->rowstoSkipatTop());
   ASSERT_TRUE(schedule->numberofHoursofData());
   EXPECT_EQ(8760, schedule->numberofHoursofData().get());
   EXPECT_EQ("Comma", schedule->columnSeparator());
   EXPECT_TRUE(schedule->interpolatetoTimestep());
-  EXPECT_EQ(60, schedule->minutesperItem());
+  ASSERT_TRUE(schedule->minutesperItem());
+  EXPECT_EQ("60", schedule->minutesperItem().get());
   EXPECT_TRUE(schedule->adjustScheduleforDaylightSavings());
 }
 
