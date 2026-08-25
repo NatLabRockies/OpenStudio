e1c026d329130b305bed1de4a36b3b49e7818d33
Kyle Benne <Kyle.Benne@nlr.gov>
2026-08-25 16:10:50 -0500

    Fix Windows application module path test

--- src/lib/test/ApplicationPathHelpers_GTest.cpp
+++ src/lib/test/ApplicationPathHelpers_GTest.cpp
@@ -17,12 +17,17 @@ TEST(ApplicationPathHelpers, Simple_test_forThisModule) {
   EXPECT_TRUE(exists(openstudioModulePath));
   // The expected path is the utilities one, but resolved for symlinks (we don't want to hardcode the version eg openstudio_utilities_tests-2.8.0)
 #if defined(_WIN32)
-#  if defined(NINJA)
-  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/openstudiolib.dll");
-#  elif _DEBUG
-  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/Debug/openstudiolib.dll");
+#  if defined(_MSC_VER)
+  constexpr auto openstudioLibraryName = "openstudioutilitieslib.dll";
 #  else
-  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/Release/openstudiolib.dll");
+  constexpr auto openstudioLibraryName = "openstudiolib.dll";
+#  endif
+#  if defined(NINJA)
+  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products") / toPath(openstudioLibraryName);
+#  elif _DEBUG
+  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/Debug") / toPath(openstudioLibraryName);
+#  else
+  openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/Release") / toPath(openstudioLibraryName);
 #  endif
 #elif __APPLE__
   openstudio::path expectedOpenstudioModulePath = getApplicationBuildDirectory() / toPath("Products/libopenstudiolib.dylib");
