56f0032a2860b3fe7218c2a7d8cded527453b179
Joe Robertson <joseph.robertson@nrel.gov>
2026-08-26 13:02:00 -0700

    Retain the ScheduleRuleset class name vs ScheduleYearRules.

--- src/energyplus/ForwardTranslator.cpp
+++ src/energyplus/ForwardTranslator.cpp
@@ -2659,7 +2659,7 @@ namespace energyplus {
       }
       case openstudio::IddObjectType::OS_Schedule_Ruleset: {
         auto mo = modelObject.cast<ScheduleRuleset>();
-        retVal = translateScheduleYearRules(mo);
+        retVal = translateScheduleRuleset(mo);
         break;
       }
       case openstudio::IddObjectType::OS_ScheduleTypeLimits: {
